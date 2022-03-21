cda4d2bdbc3a07cd763d630a0481c9cc2a00d115
chidi5 <joshdash.ji@gmail.com>
2022-03-21 13:54:36 +0100

    Home screen working

--- src/Sub/screens/AccountSetup.js
+++ src/Sub/screens/AccountSetup.js
@@ -146,6 +146,7 @@ function AccountSetup() {
                                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                         value={bank} onChange={(e) => setBank(e.target.value)}
                                         >
+                                            <option value="">Select...</option>
                                             <option value="First Bank">First Bank</option>
                                             <option value="GT Bank">GT Bank</option>
                                             <option value="Zenith Bank">Zenith Bank</option>
