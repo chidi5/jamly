6134a0014c6ef594fa19287110542d6ec76ccf1c
chidi5 <joshdash.ji@gmail.com>
2022-04-08 10:12:26 +0100

    product category

--- src/Sub/screens/ProductCategory.js
+++ src/Sub/screens/ProductCategory.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ScreenContainer from './ScreenContainer';
 
 function ProductCategory() {
     const { id } = useParams();
+    
+    const storeFront = useSelector(state => state.storeFront)
+    const { store } = storeFront
 
     return (
         <ScreenContainer>
             <div>ProductCategory {id}</div>
+            {store.store_categories.map(item => (
+                <>
+                    {item.name === id && <div> {item._id} </div>}
+                </>
+            ))}
         </ScreenContainer>
     )
 }
